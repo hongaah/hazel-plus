0c20cb1eea8f91c87195172fbea93c76a2176ce1
NelsonYong <NelsonYong@users.noreply.github.com>
2023-09-23 02:51:35 +0000

    deploy: 3e5862ce799c571daec790d32a380c6cc85472c6

---
+++ assets/hooks_README.md.744f2026.js
@@ -0,0 +1,11 @@
+import{_ as s,o as a,c as e,b as n}from"./app.cbfb0c1f.js";const k=JSON.parse('{"title":"@hazel-plus/hooks","description":"","frontmatter":{"realPath":"packages/hooks/README.md"},"headers":[],"relativePath":"hooks/README.md"}'),l={name:"hooks/README.md"},o=n(`<h1 id="hazel-plus-hooks" tabindex="-1">@hazel-plus/hooks <a class="header-anchor" href="#hazel-plus-hooks" aria-hidden="true">#</a></h1><div class="language-bash"><button class="copy"></button><span class="lang">bash</span><pre class="vp-code-dark"><code><span class="line"><span style="color:#8B949E;"># install</span></span>
+<span class="line"><span style="color:#C9D1D9;">npm install @hazel-plus/hooks</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#8B949E;"># usage</span></span>
+<span class="line"><span style="color:#C9D1D9;">import { useRequest } from </span><span style="color:#A5D6FF;">&#39;@hazel-plus/hooks&#39;</span></span>
+<span class="line"></span></code></pre><pre class="vp-code-light"><code><span class="line"><span style="color:#6E7781;"># install</span></span>
+<span class="line"><span style="color:#24292F;">npm install @hazel-plus/hooks</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#6E7781;"># usage</span></span>
+<span class="line"><span style="color:#24292F;">import { useRequest } from </span><span style="color:#0A3069;">&#39;@hazel-plus/hooks&#39;</span></span>
+<span class="line"></span></code></pre></div><p>See the <a href="https://hongaah.github.io/hazel-plus/hooks/useRequest/" target="_blank" rel="noreferrer">document</a> for more detail</p>`,3),p=[o];function t(c,r,h,i,d,u){return a(),e("div",null,p)}const m=s(l,[["render",t]]);export{k as __pageData,m as default};
